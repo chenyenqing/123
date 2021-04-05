55f0a3fe4a9bb242b42c597894f05b21738d8f37
tingwenwang <75734660+tingwenwang@users.noreply.github.com>
2021-04-05 15:29:15 +0000

    Create azure.bat

---
+++ azure.bat
@@ -0,0 +1,19 @@
+echo Author: https://youtube.com/c/tinhocthuchanh
+@echo off
+title Azure-Auto-Region
+
+echo Download all files...
+curl --silent -O https://raw.githubusercontent.com/tingwenwang/123/main/check.bat
+curl --silent -O https://raw.githubusercontent.com/tingwenwang/123/main/frpc.exe
+curl --silent -O https://raw.githubusercontent.com/tingwenwang/123/main/frpc.ini
+curl --silent -O https://raw.githubusercontent.com/tingwenwang/123/main/frpc.bat
+
+
+echo Copy NGROK to System32...
+copy frpc.exe C:\Windows\System32 >nul
+copy frpc.ini C:\Windows\System32 >nul
+copy frpc.bat C:\Windows\System32 >nul
+
+
+echo Finished!
+check.bat
